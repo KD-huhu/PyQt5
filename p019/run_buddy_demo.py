7535a5856cfcfb07c9080b90a3ee355e1eb309cb
KD-huhu <1176315241@qq.com>
2020-05-08 22:09:52 +0800

    buddy_demo

---
+++ p019/run_buddy_demo.py
@@ -0,0 +1,16 @@
+import sys
+import buddy_demo
+
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    # 创建运行程序
+    app = QApplication(sys.argv)
+    # 创建主窗口
+    mainwindow = QMainWindow()
+    ui = buddy_demo.Ui_MainWindow()
+    # 向主窗口中添加控件
+    ui.setupUi(mainwindow)
+    mainwindow.show()
+    sys.exit(app.exec_())
